98c51d64a20eba1528da7af85406c553fb5f7549
Tan Wee Keat <tanwk_97@hotmail.com>
2021-06-06 19:05:22 +0800

    Initial Commit

---
+++ ProMind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProMind
+//
+//  Created by Tan Wee Keat on 6/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
