9041ca5d3ab9a6578bd296668091b9d83b39f92e
Tan Wee Keat <tanwk_97@hotmail.com>
2021-09-27 22:55:09 +0800

    Shift speech recognition and microphone permissions to the start of the experiment

--- ProMind/Controllers/Digit Span Test/DSTGameViewController.swift
+++ ProMind/Controllers/Digit Span Test/DSTGameViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import AVFoundation
 import Speech
 import SwiftyGif
 
--- ProMind/Controllers/InitialViewController.swift
+++ ProMind/Controllers/InitialViewController.swift
@@ -6,13 +6,73 @@
 //
 
 import UIKit
+import Speech
 
 class InitialViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.performSegue(withIdentifier: K.goToExperimentProfileSegue, sender: self)
+        // Check Speech Permission
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                    // Good to go
+                    print("SFSpeechRecognizer :: Authorised!")
+                    
+                    AVAudioSession.sharedInstance().requestRecordPermission { granted in
+                        if granted {
+                            print("AVAudioSession :: Granted!")
+                            
+                            // TODO: Update time to 5 seconds
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                self.performSegue(withIdentifier: K.goToExperimentProfileSegue, sender: self)
+                            }
+                            
+                        } else {
+                            self.handlePermissionFailed(title: "This app must have access to microphone to work.", msg: "Denied")
+                        }
+                    }
+                    
+                    break
+                case .denied:
+                    // User said no
+                    print("SFSpeechRecognizer :: User denied access to speech recognition!")
+                    self.handlePermissionFailed(title: "This app must have access to speech recognition to work.", msg: "Denied")
+                    break
+                case .restricted:
+                    // Device is not permitted
+                    print("SFSpeechRecognizer :: Speech recognition restricted on this device!")
+                    self.handlePermissionFailed(title: "This app must have access to speech recognition to work.", msg: "Restricted")
+                    break
+                case .notDetermined:
+                    // Don't know yet
+                    print("SFSpeechRecognizer :: Speech recognition not yet authorised!")
+                    self.handlePermissionFailed(title: "This app must have access to speech recognition to work.", msg: "Not Determined")
+                    break
+                default:
+                    print("SFSpeechRecognizer :: Something went wrong while requesting authorisation for speech recognition!")
+                    self.handlePermissionFailed(title: "This app must have access to speech recognition to work.", msg: "Unknown")
+                }
+            }
+        }
+    }
+    
+    private func handlePermissionFailed(title: String, msg: String) {
+        // Present an alert asking the user to change their settings.
+        let ac = UIAlertController(
+            title: title,
+            message: "Code: \(msg)\n\nPlease consider updating your speech recognition and microphone settings.",
+            preferredStyle: .alert
+        )
+        ac.addAction(UIAlertAction(title: "Open settings", style: .default) { _ in
+            let url = URL(string: UIApplication.openSettingsURLString)!
+            UIApplication.shared.open(url)
+        })
+        ac.addAction(UIAlertAction(title: "Close", style: .cancel))
+        
+        DispatchQueue.main.async {
+            self.present(ac, animated: true)
         }
     }
     
