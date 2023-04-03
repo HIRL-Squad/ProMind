3316e7c36b7c27675533f04094b705a6603bf636
Yu Haikuo <e0376983@u.nus.edu>
2023-04-03 15:52:56 +0800

    Add audio record code for DST.

--- ProMind/Controllers/Digit Span Test/Models/DSTSpeechRecognitionModel.swift
+++ ProMind/Controllers/Digit Span Test/Models/DSTSpeechRecognitionModel.swift
@@ -226,4 +226,100 @@ class SFSpeechDigitNumberRecognizer: NSObject, SFSpeechRecognizerDelegate, SFSpe
     }
 }
 
-
+//private static func prepareEngine() throws -> (AVAudioEngine, SFSpeechAudioBufferRecognitionRequest) {
+//    let audioEngine = AVAudioEngine()
+//    
+//    let recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+//    recognitionRequest.shouldReportPartialResults = true
+//    
+//    let audioSession = AVAudioSession.sharedInstance()
+//    try audioSession.setCategory(.playAndRecord, mode: .spokenAudio, options: .duckOthers)
+//    try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+//    let inputNode = audioEngine.inputNode
+//    
+//    let recordingFormat = audioEngine.mainMixerNode.outputFormat(forBus: 0)
+//    
+//    // Set up a file to record to.
+//    let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+//    let recordingPath = path.appendingPathComponent("Digit Span Test Recording.caf")
+//    
+//    // Configure the
+//    var settings: [String : Any] = [:]
+//    settings[AVFormatIDKey] = kAudioFormatLinearPCM
+//    settings[AVAudioFileTypeKey] = kAudioFileCAFType
+//    settings[AVNumberOfChannelsKey] = 2
+//    settings[AVLinearPCMIsNonInterleaved] = false
+//      
+//    inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+//        recognitionRequest.append(buffer)
+//        
+//        // Configure AVAudioPCMBuffer related properties.
+//        settings[AVSampleRateKey] = buffer.format.sampleRate
+//        settings[AVLinearPCMIsFloatKey] = (buffer.format.commonFormat == .pcmFormatFloat32)
+//        
+//        let audioFile2 = try! AVAudioFile(forWriting: recordingPath, settings: buffer.format.settings)
+//        try! audioFile2.write(from: buffer)
+//        
+//        do {
+////                let audioFile = try AVAudioFile(forWriting: recordingPath, settings: settings, commonFormat: buffer.format.commonFormat, interleaved: buffer.format.isInterleaved)
+////                try audioFile.write(from: buffer)
+////                print(audioFile.length)
+////                print(audioFile.description)
+////                print(audioFile.debugDescription)
+////                print(audioFile.fileFormat)
+////                print(audioFile.url)
+//        } catch let error {
+//            print("Error happened when writting the audio file!")
+//            print(error.localizedDescription + "\n")
+//        }
+//    }
+//    audioEngine.prepare()
+//    try audioEngine.start()
+//    
+//    return (audioEngine, recognitionRequest)
+//}
+//
+//internal func transcribe() {
+//    DispatchQueue(label: "Speech Recognizer Queue", qos: .userInteractive).async { [weak self] in
+//        guard let self = self, let recognizer = self.speechRecognizer, recognizer.isAvailable else {
+//            print("Recognizer is unavailable!\n")
+//            return
+//        }
+//        do {
+//            guard let commonFormat = AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: 44100, channels: 2, interleaved: false) else {
+//                print("AVAudio Format Problem!")
+//                return
+//            }
+//            
+//            // Set up a file to record to.
+//            let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+//            if #available(iOS 16.0, *) {
+//                guard FileManager.default.isWritableFile(atPath: path.path()) else {
+//                    print("Unable to access the document directionary!\n\n\n")
+//                    return
+//                }
+//            } else {
+//                guard FileManager.default.isWritableFile(atPath: path.path) else {
+//                    print("Unable to access the document directionary!\n\n\n")
+//                    return
+//                }
+//            }
+//            
+//            let recordingPath = path.appendingPathComponent("Digit Span Test Recording.wav")
+//            let audioFile = try! AVAudioFile(forWriting: recordingPath, settings: commonFormat.settings, commonFormat: commonFormat.commonFormat, interleaved: false)
+//            
+//            // Speech Recognition + Audio Recording.
+//            let (audioEngine, request) = try Self.prepareEngine()
+//            
+//            self.audioEngine = audioEngine
+//            self.recognitionRequest = request
+//            self.recognitionTask = recognizer.recognitionTask(with: request, resultHandler: self.recognitionHandler(result:error:))
+//            
+//            /// recognitionTask creation through delegate method is unable to work as expected.
+//            // self.recognitionTask = recognizer.recognitionTask(with: request, delegate: self)
+//        } catch {
+//            self.reset()
+//            print("Error happens for speech recognizer: \(error)")
+//        }
+//    }
+//}
