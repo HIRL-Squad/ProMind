7527d00c44fc83c05d43ea326f8189cdb58ca810
Yu Haikuo <e0376983@u.nus.edu>
2023-04-29 13:21:34 +0800

    Rearrange file structure.

---
+++ ProMind/Controllers/Test Results/TestRecordModificationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TestRecordModificationViewController.swift
+//  ProMind
+//
+//  Created by HAIKUO YU on 21/4/23.
+//
+
+import UIKit
+
+class TestRecordModificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
